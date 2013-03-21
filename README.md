c7cd512b159bf76c5aef979384a934409ee148dd
Nikolay Ivanushkin <nikolay.ivanushkin@dev.nettv.staging.ayyo.ru>
2013-03-21 21:11:07 +0400

    Добавлена поддержка объектов и новых параметров

--- README.md
+++ README.md
@@ -11,8 +11,26 @@ Methods
 ```js
 var mac_notify = require('mac_notify');
 
-mac_notify.send('Message', 'Header');
+//attributes
+mac_notify.send('Title', 'subtitle', 'message', 'group');
+
+//object
+mac_notify.send({title: 'Title', subtitle: 'subtitle', message: 'message', group: 'group'});
 ```
+
+Defaults
+ * **title** — Notification
+ * **subtitle** — NOW_DATE
+ * **message** — Nothing
+ * **group** — ALL
+
+If you specify empty string (''), attribute will be turned off.
+If you padd *null* or *undefined*, attribute will be default.
+
+Specifies the ‘group’ a notification belongs to. For any ‘group’ only _one_
+notification will ever be shown, replacing previously posted notifications.
+See terminal-notifier [documentation](https://github.com/alloy/terminal-notifier/blob/master/README.markdown).
+
 
 ©
 -----
--- app/app.js
+++ app/app.js
@@ -1,14 +1,40 @@
 var exec = require("child_process").exec;
 var path = require("path");
 
-exports.send = function(message, header){
+exports.send = function(){
     
-    if(header == null){
-        var header = 'Message'
+    options = {
+        title: 'Notification',
+        subtitle: new Date().toJSON(),
+        message: 'Nothing',
+        group: 'ALL'
+    }
+    
+    if( typeof(arguments[0]) == 'object' ){
+        for(argument in arguments[0]){
+            if(options[argument] != null && arguments[0] != undefined){
+                options[argument] = arguments[0][argument]
+            }
+        }
+    } else {
+        var i = 0;
+        for(option in options){
+            if(arguments[i] != null && arguments[i] != undefined){
+                options[option] = arguments[i];
+            }
+            i++;
+        }
+    }
+
+    var attrs = ''
+    for(option in options){
+        if(options[option] != ''){
+            attrs += ' -' + option + ' "' + options[option] + '"';
+        }
     }
     
     var command = path.join(__dirname, '../lib/terminal-notifier.app/Contents/MacOS/terminal-notifier');
-    command += ' -message "' + message + '" -title "' + header + '"'
+    command += attrs
     
     exec(command, function(e){
         if(e){
