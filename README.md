c30b52900a6a81afd4d836cee13dbe72cbfcfe6a
Nikolay Ivanushkin <nikolay.ivanushkin@dev.nettv.staging.ayyo.ru>
2013-03-21 17:23:42 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+MacOS notify
+============
+
+Notifications from Node.js apps.
+
+Methods
+-------
+
+**send**
+
+```js
+var mac_notify = require('mac_notify');
+
+mac_notify.send('Message', 'Header');
+```
---
+++ app/app.js
@@ -0,0 +1,17 @@
+var exec = require("child_process").exec;
+var path = require("path");
+
+exports.send = function(message, header){
+    
+    if(header == null){
+        var header = 'Message'
+    }
+    
+    var command = path.join(__dirname, '../lib/terminal-notifier.app/Contents/MacOS/terminal-notifier');
+    command += ' -message "' + message + '" -title "' + header + '"'
+    
+    exec(command, function(e){
+        console.log(e);
+        console.log('Sended!');
+    });
+}
