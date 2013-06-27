2ce370953bab14cc4ae7152c6b541328f4853268
Nikolay Ivanushkin <nikolay.ivanushkin@inn.ru>
2013-06-27 13:40:10 +0400

    Support other platforms

--- app/app.js
+++ app/app.js
@@ -1,26 +1,25 @@
 var exec = require("child_process").exec;
 var path = require("path");
+var os   = require("os").platform();
 
-exports.send = function(){
+exports.send = function(newOptions){
     
     options = {
         title: 'Notification',
         subtitle: new Date().toJSON(),
         message: 'Nothing',
-        group: 'ALL'
+        group: 'ALL',
+        
+        urgency: 'low',
+        expireTime: 1000,
+        icon: null
     }
     
-    if( typeof(arguments[0]) == 'object' ){
-        for(argument in arguments[0]){
-            options[argument] = arguments[0][argument]
-        }
-    } else {
-        var i = 0;
-        for(option in options){
-            if(typeof arguments[i] !== 'undefined' && arguments[i] != null){
-                options[option] = arguments[i];
+    if( typeof(newOptions) == 'object' ){
+        for(argument in newOptions){
+            if(newOptions[argument]){
+                options[argument] = newOptions[argument]
             }
-            i++;
         }
     }
 
@@ -30,15 +29,48 @@ exports.send = function(){
             attrs += ' -' + option + ' "' + options[option] + '"';
         }
     }
-    
-    var command = "'"+path.join(__dirname, '../lib/terminal-notifier.app/Contents/MacOS/terminal-notifier')+"'";
-    command += attrs
-    
+
+    var command = '';
+    switch(os){
+        case 'darwin':
+            command = "'"+path.join(__dirname, '../lib/terminal-notifier.app/Contents/MacOS/terminal-notifier')+"'";
+            command += macAttr(options);
+            break;
+        case 'win32':
+            command = "'"+path.join(__dirname, '../lib/notify-send.exe')+"'";
+            command += linuxAttr(options);
+            break;
+        default:
+            command = 'notify-send';
+            command += linuxAttr(options);
+            break;
+    }
+
     exec(command, function(e){
         if(e){
-            console.log('Error', e);
+            console.log('Error!', e);
         } else {
             console.log('Sended!');
         }
     });
+}
+
+function macAttr(options){
+    var attrs = ''
+    if(options.title) attrs += ' -title "' + options.title + '"';
+    if(options.subtitle) attrs += ' -subtitle "' + options.subtitle + '"';
+    if(options.message) attrs += ' -message "' + options.message + '"';
+    if(options.group) attrs += ' -group "' + options.group + '"';
+    return attrs;
+}
+
+function linuxAttr(options){
+    var attrs = ''
+    if(options.title) attrs += ' "' + options.title + '"';
+    if(options.message) attrs += ' "' + options.message + '"';
+    if(options.group) attrs += ' -c "' + options.group + '"';
+    if(options.urgency) attrs += ' -t "' + options.urgency + '"';
+    if(options.expireTime) attrs += ' -u "' + options.expireTime + '"';
+    if(options.icon) attrs += ' -i "' + options.icon + '"';
+    return attrs;
 }
